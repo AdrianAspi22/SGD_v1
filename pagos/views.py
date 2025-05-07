948369c78d58ea5ab666d0d4124ff7e7d3347023
Aldana Arleth Medina Ortiz-Ing. en Informatica y Sistemas <aldana.medina@unas.edu.pe>
2025-05-07 00:36:44 -0500

    olita, mi primer cambio

--- pagos/views.py
+++ pagos/views.py
@@ -8,7 +8,6 @@ from django.contrib import messages
 # Vista para listar los pagos
 @login_required
 def listar_pagos(request):
-    # Filtramos los pagos por el dojo del usuario autenticado
     pagos = Pago.objects.filter(dojo=request.user.dojo)
     return render(request, 'pagos/listar_pagos.html', {'pagos': pagos})
 
@@ -18,27 +17,26 @@ def crear_pago(request):
     if request.method == 'POST':
         form = PagoForm(request.POST)
         if form.is_valid():
-            # Asignamos el dojo actual del usuario al nuevo pago
             pago = form.save(commit=False)
-            pago.dojo = request.user.dojo  # Asignamos el dojo del usuario autenticado
+            pago.dojo = request.user.dojo
             pago.save()
-            messages.success(request, 'Pago creado con éxito')
+            messages.success(request, 'Pago creado con éxito.')
             return redirect('listar_pagos')
     else:
         form = PagoForm()
     return render(request, 'pagos/crear_pago.html', {'form': form})
 
-# Vista para editar un pago existente
+# En la vista de editar_pago
 @login_required
 def editar_pago(request, pk):
-    dojo = request.user.dojo  # acceso directo al dojo del usuario
+    dojo = request.user.dojo  # Accedemos al dojo del usuario
     pago = get_object_or_404(Pago, id=pk, dojo=dojo)
 
     if request.method == 'POST':
         form = PagoForm(request.POST, instance=pago)
         if form.is_valid():
             pago = form.save(commit=False)
-            pago.dojo = dojo  # asegúrate de mantener la relación
+            pago.dojo = dojo  # Aseguramos que el dojo se mantenga correctamente
             pago.save()
             messages.success(request, 'Pago actualizado con éxito')
             return redirect('listar_pagos')
@@ -47,12 +45,16 @@ def editar_pago(request, pk):
 
     return render(request, 'pagos/editar_pago.html', {'form': form})
 
-# Vista para eliminar un pago
+
+# Vista para eliminar un pago existente
 @login_required
 def eliminar_pago(request, pago_id):
-    pago = get_object_or_404(Pago, id=pago_id)
+    dojo = request.user.dojo
+    pago = get_object_or_404(Pago, id=pago_id, dojo=dojo)
+
     if request.method == 'POST':
         pago.delete()
-        messages.success(request, 'Pago eliminado con éxito')
-        return redirect('lista_pagos')
+        messages.success(request, 'Pago eliminado con éxito.')
+        return redirect('listar_pagos')
+
     return render(request, 'pagos/eliminar_pago.html', {'pago': pago})
