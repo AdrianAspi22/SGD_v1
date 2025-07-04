46c4e1ef1d42748e395978b66d4240262b3332fc
Aldana Arleth Medina Ortiz <aldana.medina.dft.team@gmail.com>
2025-07-04 17:34:26 -0500

    validacion_edad

--- alumnos/forms.py
+++ alumnos/forms.py
@@ -2,6 +2,8 @@
 
 from django import forms
 from .models import Alumno
+from datetime import date
+from django.core.exceptions import ValidationError
 
 class AlumnoForm(forms.ModelForm):
     fecha_nacimiento = forms.DateField(
@@ -14,3 +16,17 @@ class AlumnoForm(forms.ModelForm):
             'nombre', 'apellido', 'fecha_nacimiento',
             'matricula', 'grupo', 'estado'
         ]
+
+    def clean_fecha_nacimiento(self):
+        fecha_nacimiento = self.cleaned_data['fecha_nacimiento']
+        hoy = date.today()
+        edad = hoy.year - fecha_nacimiento.year - ((hoy.month, hoy.day) < (fecha_nacimiento.month, fecha_nacimiento.day))
+
+        if edad < 5:
+            raise ValidationError('El alumno no puede ser menor de 5 años.')
+        elif edad > 60:
+            raise ValidationError('El alumno no puede ser mayor de 60 años.')
+
+        return fecha_nacimiento
+
+
